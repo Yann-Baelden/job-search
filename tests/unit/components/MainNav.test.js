1a58038b6548fe323b68d6fe4883157c62f620b5
Yann Baelden <yann.baelden@ubalia.fr>
2023-01-06 20:35:43 +0100

    mise en place du projet, du testing...

---
+++ tests/unit/components/MainNav.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/vue";
+
+import MainNav from "@/components/MainNav.vue";
+
+describe("MainNav", () => {
+  it("displays company name", () => {
+    render(MainNav);
+    const companyName = screen.getByText("Bobo Careers");
+    expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    console.log(navigationMenuTexts);
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo Corp",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
+});
