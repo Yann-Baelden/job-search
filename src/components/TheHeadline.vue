406ecd830aad45dfc1ad3491e8dfbe99da15a4d3
Yann Baelden <yann.baelden@ubalia.fr>
2023-02-02 16:25:03 +0100

    add css to the headline

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,11 +1,11 @@
 <template>
   <section>
-    <h1>
-      <span :class="actionClasses">{{ action }}</span
-      ><br />
+    <h1 class="mb-14 text-8xl font-bold tracking-tighter">
+      <span :class="actionClasses">{{ action }}</span>
+      <br />
       for everyone
     </h1>
-    <h2>Find your next job at Bobo Corp.</h2>
+    <h2 class="text-3xl font-light">Find your next job at Bobo Corp.</h2>
   </section>
 </template>
 
